bcf323e34c8bb8d8dca87eb8f4ad34fab6283568
Alessandro Araujo <alessandro.v.b.araujo@gmail.com>
2025-05-10 14:04:12 -0300

    Ajustes e tipagem

--- src/app/Interfaces/Database.php
+++ src/app/Interfaces/Database.php
@@ -10,10 +10,28 @@ use PDO;
  */
 interface Database
 {
-    public function select(string $config_search = 'fetchAll', string $config_param = PDO::FETCH_ASSOC): array;
+    /**
+     * @param string $config_search
+     * @param int $config_param (default: PDO::FETCH_ASSOC)
+     * @return array<int, array<string, mixed>> The result set as an associative array
+     */
+    public function select(string $config_search = 'fetchAll', int $config_param = PDO::FETCH_ASSOC): array;
+
+    /**
+     * @param  array<int, array<string, mixed>> $columns
+     * @return self
+     */
     public function columns(array $columns): self;
-    public function where(string $field, $value, string $operator = '='): self;
-    public function orWhere(string $field, $value, string $operator = '='): self;
+
+    /** PARAMETROS DE CONFIGURAÇÃO
+     * @param string $field
+     * @param string $field
+     * @param string $value
+     * @param string $operator
+     * @return self
+     */
+    public function where(string $field, string $value, string $operator = '='): self;
+    public function orWhere(string $field, string $value, string $operator = '='): self;
     public function table(string $table): self;
     public function join(string $table, string $firstField, string $secondField, string $joinType = 'INNER'): self;
 }
