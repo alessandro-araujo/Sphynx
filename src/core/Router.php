d788f385b9ca2b31a895ddf3cdce15c1ca6ae184
Alessandro Araujo <alessandro.v.b.araujo@gmail.com>
2025-05-11 01:18:03 -0300

    Implementando Melhorias na Router

--- src/core/Router.php
+++ src/core/Router.php
@@ -2,46 +2,77 @@
 
 namespace Core;
 
-class Router
-{
+class Router {
     private static ?Router $instance = null;
+     /**
+      * @var array<string, array<string, array{handler: callable|array{0: class-string, 1: string}, middleware?: string|null, args?: array<string|int, mixed>}>>
+      */
     private array $routes = [];
 
-    private function __construct()
-    {
+    private function __construct() {
         register_shutdown_function([$this, 'handleRequest']);
     }
 
-    public static function getInstance(): Router
-    {
+    public static function getInstance(): Router {
         if (self::$instance === null) {
             self::$instance = new Router();
         }
         return self::$instance;
     }
 
-    public static function get(string $path, array|callable $handler, ?string $middleware = null): void
-    {
-        self::getInstance()->addRoute('GET', $path, $handler, $middleware);
+    /**
+     * @param string $path
+     * @param callable|array{0: class-string, 1: string} $handler
+     * @param string|null $middleware
+     * @param array<string|int, mixed> $args
+     * @return void
+     */
+    public static function get(string $path, array|callable $handler, ?string $middleware = null, array $args = []): void {
+        self::getInstance()->addRoute('GET', $path, $handler, $middleware, $args);
     }
 
-    public static function post(string $path, array|callable $handler, ?string $middleware = null, array $args = []): void
-    {
+     /**
+      * @param string $path
+      * @param callable|array{0: class-string, 1: string} $handler
+      * @param string|null $middleware
+      * @param array<string|int, mixed> $args
+      * @return void
+      */
+    public static function post(string $path, array|callable $handler, ?string $middleware = null, array $args = []): void {
         self::getInstance()->addRoute('POST', $path, $handler, $middleware, $args);
     }
 
-    public static function put(string $path, array|callable $handler, ?string $middleware = null): void
-    {
-        self::getInstance()->addRoute('PUT', $path, $handler, $middleware);
+    /**
+     * @param string $path
+     * @param callable|array{0: class-string, 1: string} $handler
+     * @param string|null $middleware
+     * @param array<string|int, mixed> $args
+     * @return void
+     */
+    public static function put(string $path, array|callable $handler, ?string $middleware = null, array $args = []): void {
+        self::getInstance()->addRoute('PUT', $path, $handler, $middleware, $args);
     }
 
-    public static function delete(string $path, array|callable $handler, ?string $middleware = null): void
-    {
-        self::getInstance()->addRoute('DELETE', $path, $handler, $middleware);
+    /**
+     * @param string $path
+     * @param callable|array{0: class-string, 1: string} $handler
+     * @param string|null $middleware
+     * @param array<string|int, mixed> $args
+     * @return void
+     */
+    public static function delete(string $path, array|callable $handler, ?string $middleware = null, array $args = []): void {
+        self::getInstance()->addRoute('DELETE', $path, $handler, $middleware, $args);
     }
 
-    private function addRoute(string $method, string $path, array|callable $handler, ?string $middleware = null, array $args = []): void
-    {
+     /**
+      * @param string $method
+      * @param string $path
+      * @param callable|array{0: class-string, 1: string} $handler
+      * @param string|null $middleware
+      * @param array<string|int, mixed> $args
+      * @return void
+      */
+    private function addRoute(string $method, string $path, array|callable $handler, ?string $middleware = null, array $args = []): void {
         $this->routes[$method][$path] = [
             'handler' => $handler,
             'middleware' => $middleware,
@@ -49,26 +80,26 @@ class Router
         ];
     }
 
-    public function handleRequest(): void
-    {
-        $method = $_SERVER['REQUEST_METHOD'];
-        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    public function handleRequest(): void {
+         /** @var string $method */      $method = $_SERVER['REQUEST_METHOD'];
+         /** @var string $request_uri */ $request_uri = $_SERVER['REQUEST_URI'];
+         /** @var string $uri */         $uri = parse_url($request_uri, PHP_URL_PATH);
 
         if (!isset($this->routes[$method])) {
             $this->sendNotFound();
             return;
         }
 
-        foreach ($this->routes[$method] as $route => $routeData) {
-            $pattern = preg_replace('/\{([a-zA-Z0-9_]+)\}/', '([^/]+)', rtrim($route, '/') . '/*');
-            if (preg_match("#^{$pattern}$#", rtrim($uri, '/'), $matches)) {
+        foreach ($this->routes[$method] as $route => $route_data) {
+            $pattern = preg_replace('/\{([a-zA-Z0-9_]+)}/', '([^/]+)', rtrim($route, '/') . '/*');
+            if (preg_match("#^$pattern$#", rtrim($uri, '/'), $matches)) {
                 array_shift($matches);
 
-                $handler = $routeData['handler'];
-                $middleware = $routeData['middleware'] ?? null;
-                $args = $routeData['args'] ?? [];
+                $handler = $route_data['handler'];
+                $middleware = $route_data['middleware'] ?? null;
+                $args = $route_data['args'] ?? [];
                 $inputData = ($method === 'POST' || $method === 'PUT')
-                    ? json_decode(file_get_contents('php://input'), true)
+                    ? json_decode(file_get_contents('php://input') ?: '', true)
                     : [];
 
                 $callable = function () use ($handler, $inputData, $matches, $args) {
@@ -76,17 +107,18 @@ class Router
                         call_user_func_array($handler, [$inputData, ...$matches, $args]);
                     } else {
                         [$controller, $action] = $handler;
+                        /** @phpstan-ignore-next-line */
                         call_user_func_array([new $controller(), $action], [$inputData, ...$matches, $args]);
                     }
                 };
 
                 if ($middleware) {
                     $middlewareInstance = new $middleware();
+                    /** @phpstan-ignore-next-line */
                     $middlewareInstance->handle($callable);
                 } else {
                     $callable();
                 }
-
                 return;
             }
         }
@@ -94,8 +126,7 @@ class Router
         $this->sendNotFound();
     }
 
-    private function sendNotFound(): void
-    {
+    private function sendNotFound(): void {
         http_response_code(404);
         echo json_encode(["error" => "Rota não encontrada"]);
     }
