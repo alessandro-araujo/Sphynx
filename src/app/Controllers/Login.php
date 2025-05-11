986b3ef966559452f07d5d526ecac31149acc02a
Alessandro Araujo <alessandro.v.b.araujo@gmail.com>
2025-05-11 00:37:02 -0300

    Melhorias na controller Login.php

--- src/app/Controllers/Login.php
+++ src/app/Controllers/Login.php
@@ -4,38 +4,47 @@ namespace App\Controllers;
 
 use App\Models\User;
 use App\Helpers\JWTHandler;
-
+use Database\InlineSQL;
+use Exception;
 class Login extends Controller {
     /**
     * @param array{email: string, password: string} $request
-    * @param array{connection: \Database\InlineSQL} $args
-    */
+    * @param array{connection: InlineSQL} $args
+     */
     public function login(array $request, array $args): void {
         if (empty($request['email']) || empty($request['password'])) $this->response(["error" => "E-mail ou senha não fornecidos"], 400);
 
         $user_model = new User($args['connection']);
+        /** @var array{status: string, message?: string, result?: array{id: int, email: string, username: string, password: string}} $user */
+        
         $user = $user_model->login($request['email']);
+        $user_status = $user['status'];
 
-        /** @var array{error?: bool, message?: string} $user */
-        if (isset($user['error'])) {
+        if ($user_status === 'error') {
             assert(isset($user['message']));
             $this->response(["error" => $user['message'] . " de Login"], 401);
         }
-        
-        /** @var array{id: int, email: string, username: string, password: string} $user */
-        if (!password_verify($request['password'], $user['password'])) $this->response(["error" => "E-mail ou senha inválidos"], 401);
-        
+        if (isset($user['result'])) $login = $user['result']; assert(isset($login));
+        unset($user);
+
+        /** @var array{id: int, email: string, username: string, password: string} $login */
+        if (!password_verify($request['password'], $login['password'])) {
+            $this->response(["error" => "E-mail ou senha inválidos"], 401);
+        }
+
+        unset($request);
+
         $jwtHandler = new JWTHandler();
         $payload = [
-            'sub' => $user['id'],
-            'email' => $user['email'],
-            'username' => $user['username']
+            'sub' => $login['id'],
+            'email' => $login['email'],
+            'username' => $login['username']
         ];
         
         try {
             $jwt = $jwtHandler->gerarToken($payload);
             $this->response(["message" => "Login bem-sucedido!", "token" => $jwt], 200);
-        } catch (\Exception $error) {
+        } catch (Exception $error) {
             if (($_ENV['APP_ENV'] == 'development') AND ($_ENV['APP_DEBUG'] == 'True')) $this->response(["error" => $error->getCode() .' '. $error->getMessage()], 401);
             $this->response(["error" => "Ocorreu um erro na autenticação: "], 401);
         }
