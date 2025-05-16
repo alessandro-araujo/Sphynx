b413c91f4c23cc98a6c712604496ea79ee0eae84
Alessandro Araujo <alessandro.v.b.araujo@gmail.com>
2025-05-16 02:31:53 -0300

    Fix Ajustes da controler
    Signed-off-by: Alessandro Araujo <alessandro.v.b.araujo@gmail.com>

--- src/app/Controllers/Controller.php
+++ src/app/Controllers/Controller.php
@@ -20,6 +20,5 @@ class Controller {
         http_response_code($statusCode);
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($data);
-        return;
     }
 }
