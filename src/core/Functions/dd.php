8d96d7c069e0d9c33b9ca973572b2f8bcf6a7241
Alessandro Araujo <alessandro.v.b.araujo@gmail.com>
2025-06-06 19:37:55 -0300

    Função de debug

--- src/core/Functions/dd.php
+++ src/core/Functions/dd.php
@@ -1,8 +1,28 @@
 <?php
 
-function dd($array): void {
-    print('<pre>');;
-    print_r($array);
-    print('</pre>');
-    die;
+// function dd($array): void {
+//     print('<pre>');;
+//     print_r($array);
+//     print('</pre>');
+//     die;
+// }
+// function dd(...$args): void {
+//     echo '<pre>';
+//     foreach ($args as $arg) {
+//         print_r($arg);
+//     }
+//     echo '</pre>';
+//     die;
+// }
+
+
+// @phpstan-ignore missingType.iterableValue
+function dd(array ...$vars): void {
+    echo '<pre style="background:#222;color:#0f0;padding:10px;">';
+    foreach ($vars as $var) {
+        var_dump($var);
+        echo "\n";
+    }
+    echo '</pre>';
+    exit;
 }
