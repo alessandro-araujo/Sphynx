c51dd8f3772e8588ca264db19bc09435bf965fa6
Alessandro Araujo <alessandro.v.b.araujo@gmail.com>
2025-06-14 11:43:48 -0300

    refactoring Helpers to the new project design pattern
    Signed-off-by: Alessandro Araujo <alessandro.v.b.araujo@gmail.com>

---
+++ src/app/Helpers/JWT.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Helpers;
+
+final class JWT {
+    /**
+     * @param array{id: int|string, email: string, username: string} $login
+     * @return string
+     */
+    public static function create(array $login): string {
+        $jwt = new JWTHandler();
+        $payload = [
+            'sub' => $login['id'],
+            'email' => $login['email'],
+            'username' => $login['username']
+        ];
+        return $jwt->gerarToken($payload);
+    }
+}
--- src/app/Helpers/Result.php
+++ src/app/Helpers/Result.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 namespace App\Helpers;
 
 final class Result {
@@ -11,6 +11,17 @@ final class Result {
         if ($data['status'] === 'error') {
             return false;
         }
+        return true;
+    }
+
+    /**
+     * @param string $password
+     * @param string $login_password
+     * @return bool
+     */
+    public static function password(string $password, string $login_password): bool {
+        if (empty($login_password)) return false;
+        if (!password_verify($password, $login_password)) return false;
         return true;
     }
 }
